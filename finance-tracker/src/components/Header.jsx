3fff3921e64aa7fc6803ed4e3e9522414546990b
Duck-Dev687 <duckdev32@gmail.com>
2024-10-12 12:35:35 +0300

    Added structer to Header.jsx & created _header.scss

---
+++ finance-tracker/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './_header.scss';
+
+const Header = () => (
+  <header className="header">
+    <h1>Finance Tracker</h1>
+  </header>
+);
+
+export default Header;
