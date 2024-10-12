935b79ac9c93339228269d003e5b9944b2b40277
Duck-Dev687 <duckdev32@gmail.com>
2024-10-12 16:48:13 +0300

    some changes to App.jsx

--- finance-tracker/src/App.jsx
+++ finance-tracker/src/App.jsx
@@ -1,17 +1,15 @@
-// src/App.jsx
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import TransactionList from './components/TransactionList';
 import AddTransactionForm from './components/AddTransactionForm';
 import AccountStatement from './components/AccountStatement'; // Import the new component
-import { useTheme } from './contexts/ThemeContext'; // useTheme imported from ThemeContext
+import { useTheme } from './contexts/ThemeContext';
 import './SCSS/styles.scss';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
-  // Custom hook for managing local storage
   const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
       const saved = localStorage.getItem(key);
@@ -26,26 +24,23 @@ const App = () => {
   };
 
   const [transactions, setTransactions] = useLocalStorage('transactions', []);
-  
-  const addTransaction = (transaction) => {
-    // Ensure the amount is negative for withdrawals
-    if (transaction.type === 'withdrawal') {
-      transaction.amount = -Math.abs(transaction.amount);
-    } else {
-      transaction.amount = Math.abs(transaction.amount);
-    }
-    setTransactions([...transactions, transaction]);
-  };
-
   const [userInfo, setUserInfo] = useState({
     name: 'User Name', // Replace with user input or fetch from a user profile
     country: 'USA', // Replace with user input
     city: 'New York', // Replace with user input
     timezone: 'GMT-5', // Replace with user input or fetch the user's timezone
   });
-  // const addTransaction = (transaction) => {
-  //   setTransactions([...transactions, transaction]);
-  // };
+
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const totalBalance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+    setBalance(totalBalance);
+  }, [transactions]);
+
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
 
   return (
     <div className={`app ${theme}`}>
