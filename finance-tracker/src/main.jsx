b7f09370807ddecdc9bbb1ca7946d989ad1f0b5e
Duck-Dev687 <duckdev32@gmail.com>
2024-10-12 12:54:36 +0300

    import './SCSS/styles.scss'

--- finance-tracker/src/main.jsx
+++ finance-tracker/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './SCSS/styles.scss'
 import './assets/modernizr-custom.js';
 
 createRoot(document.getElementById('root')).render(
